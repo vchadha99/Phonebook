0476566d1ad516f4fd30893cde32ea5b45007af0
Varun <vchadha99@gmail.com>
2020-05-31 17:39:54 +0530

    added contacts list page

--- controllers/phonebookController.js
+++ controllers/phonebookController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Phonebook = mongoose.model('phonebook');
 
 router.get('/',(req , res) => {
     res.render('phonebook/phoneBookForm');
@@ -12,5 +14,98 @@ router.post('/', (req, res) => {
         else
         updateRecord(req, res);
 });
+
+function insertRecord(req, res) {
+    var phonebook = new Phonebook();
+    phonebook.fullName = req.body.fullName;
+    phonebook.email = req.body.email;
+    phonebook.altEmail = req.body.atlEmail;
+    phonebook.mobile = req.body.mobile;
+    phonebook.alMobile = req.body.altMobile;
+    phonebook.city = req.body.city;
+    phonebook.save((err, doc) => {
+        if (!err)
+            res.redirect('phonebook/list');
+        else {
+            if (err.name == 'ValidationError') {
+                console.log('give correct details');
+                handleValidationError(err, req.body);
+                res.render('phonebook/phoneBookForm', {
+                    viewTitle: "Insert Contact",
+                    phonebook: req.body
+                });
+            }
+            else
+                console.log('Error during record insertion : ' + err);
+        }
+    });
+}
+
+function updateRecord(req, res) {
+    Phonebook.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, doc) => {
+        if (!err) { res.redirect('phonebook/list'); }
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("phonebook/list", {
+                    viewTitle: 'Update Contact',
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record update : ' + err);
+        }
+    });
+}
+
+router.get('/list', (req, res) => {
+    Phonebook.find((err, docs) => {
+        if (!err) {
+            res.render("phonebook/list", {
+                list: docs
+            });
+        }
+        else {
+            console.log('Error in retrieving phonebook :' + err);
+        }
+    });
+});
+
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+router.get('/:id', (req, res) => {
+    Phonebook.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("phonebook/phoneBookForm", {
+                viewTitle: "Update Contact",
+                employee: doc
+            });
+        }
+    });
+});
+
+router.get('/delete/:id', (req, res) => {
+    Phonebook.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) {
+            res.redirect('/phonebook/list');
+        }
+        else { console.log('Error in contact delete :' + err); }
+    });
+});
+
 
 module.exports = router;
--- models/phonebook.model.js
+++ models/phonebook.model.js
@@ -21,4 +21,9 @@ var phonebookSchema = new mongoose.Schema({
     }
 });
 
+phonebookSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
 mongoose.model('phonebook', phonebookSchema);
